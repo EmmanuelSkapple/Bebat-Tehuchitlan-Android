fdfdc380f10d2fca9004322bedb7850d77b817bd
robert HP <roberto.karlos.lopez@gmail.com>
2018-05-21 02:35:31 -0500

    el mapfragment de la informacion del lugar carga correctamente la ubicacion del lugar que se selecciono, asi como la imagen correspondiente del lugar y se cambio el color del fondo del layout

--- app/src/main/java/com/example/adan/teuchitlan/informacionLugar.java
+++ app/src/main/java/com/example/adan/teuchitlan/informacionLugar.java
@@ -4,9 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class informacionLugar extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class informacionLugar extends AppCompatActivity implements OnMapReadyCallback {
 
 
     String nombre;
@@ -20,7 +28,7 @@ public class informacionLugar extends AppCompatActivity {
     TextView tv_nombre;
     TextView tv_descripcion;
     TextView tv_id;
-    TextView tv_imagenes;
+    ImageView iv_imagenes;
     TextView tv_key;
     TextView tv_tipo;
     TextView tv_ubicacion;
@@ -44,11 +52,18 @@ public class informacionLugar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion_lugar);
 
+        //inicializa el mapa
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapa_informacion_lugar_id);
+        mapFragment.getMapAsync(this);
+
         tv_nombre = (TextView) findViewById(R.id.nombre_informacion_lugar_id);
         tv_descripcion = (TextView) findViewById(R.id.descripcion_informacion_lugar_id);
+        iv_imagenes = (ImageView) findViewById(R.id.imagen_lugar_id);
 
         tv_nombre.setText(nombre);
         tv_descripcion.setText(descripcion);
+        new DownLoadImageTask(iv_imagenes).execute(imagenes);
 
 
         Log.d("recibio el nombre", nombre);
@@ -60,5 +75,20 @@ public class informacionLugar extends AppCompatActivity {
         Log.d("recibio la ubicacion", ubicacion);
 
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        String [] coordenadas = ubicacion.split(",");
+
+        Double Latitud = Double.parseDouble(coordenadas[0]);
+        Double Longitud = Double.parseDouble(coordenadas[1]);
+
+        LatLng ubicacion = new LatLng( Latitud, Longitud);
+        googleMap.addMarker(new MarkerOptions().position(ubicacion)
+                .title(nombre));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion, 15.0f));
     }
 }
